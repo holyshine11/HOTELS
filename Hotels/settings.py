cc392ddf21bc3ed434aee3a85048293513a7b5a1
ScottPark <53296958+holyshine11@users.noreply.github.com>
2023-08-01 13:32:13 +0900

    Update settings.py

--- Hotels/settings.py
+++ Hotels/settings.py
@@ -134,4 +134,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/media/midia/'
+MEDIA_URL = '/media/media/image/'
