65ab422804276fa9bb6ab651e72ef57261ca48f0
ScottPark <53296958+holyshine11@users.noreply.github.com>
2023-08-01 11:34:16 +0900

    Update settings.py
    fixed hosts

--- Hotels/settings.py
+++ Hotels/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gn4tr^tdy2g)71na7hs#t_guj-es0n(%^4*922wr6!taj&dn87
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['port-0-hotels-ac2nlkrnfuqm.sel4.cloudtype.app']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
