199f49df74f632352b443d8692fb59750865ebb4
scottpark <holyshine11@naver.com>
2023-08-02 11:58:07 +0900

    씽크맞추기

--- Hotels/settings.py
+++ Hotels/settings.py
@@ -148,4 +148,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'Hotels/media/midia'
+MEDIA_URL = '/midia/'
--- Hotels/urls.py
+++ Hotels/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('room_service.urls')),
     path('room_service/', include('room_service.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
